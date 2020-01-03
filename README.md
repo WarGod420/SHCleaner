bb190f87d950c3df7060182a3899deca24f2e5b8
SarahH12099 <sarahh12099@gmail.com>
2020-01-02 19:49:12 -0500

    Update SHCleaner to 1.3.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SHCleaner
 A bat file PC Cleaner for Windows 10
 <br><br>
-Latest Version: 1.2.9
+Latest Version: 1.3.0
 <br><br>
 Features:
 - Internet Cleaner (History, Cache, Logs, Etc. Doesn't clean saved passwords).
--- SHCleaner.bat
+++ SHCleaner.bat
@@ -1,5 +1,5 @@
 @echo off
-title SHCleaner v1.2.9 By SarahH12099
+title SHCleaner v1.3.0 By SarahH12099
 
 MODE 107,25
 
@@ -52,7 +52,7 @@ if not errorlevel 1 (
 cd \>nul 2>&1
 cls
 echo -----------------------------------------------------------------------------------------------------------
-echo SHCleaner v1.2.9
+echo SHCleaner v1.3.0
 echo Made By SarahH12099
 echo -----------------------------------------------------------------------------------------------------------
 echo.
@@ -796,7 +796,7 @@ FOR /D %%p IN ("%localappdata%\Reflector 3\*.*") DO rmdir "%%p" /s /q>nul 2>&1
 :: Revo Uninstaller Pro
 if exist "%localappdata%\VS Revo Group\Revo Uninstaller Pro\Logs" (
 taskkill /F /IM "RevoUninPro.exe">nul 2>&1
-del /q /s /f %localappdata%\VS Revo Group\Revo Uninstaller Pro\Logs\*.*">nul 2>&1
+del /q /s /f "%localappdata%\VS Revo Group\Revo Uninstaller Pro\Logs\*.*">nul 2>&1
 FOR /D %%p IN ("%localappdata%\VS Revo Group\Revo Uninstaller Pro\Logs\*.*") DO rmdir "%%p" /s /q>nul 2>&1
 )>nul 2>&1
 
@@ -926,7 +926,7 @@ For /f "skip=1 Delims=" %%# in (
 ) Do If not defined MD5 Set MD5=%%#
 Set MD5=%MD5: =%
 
-set Version=1.2.9
+set Version=1.3.0
 cd %temp%>nul 2>&1
 
 if exist "sqlite3md5.txt" (
